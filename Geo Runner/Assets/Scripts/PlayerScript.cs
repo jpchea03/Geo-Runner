d37f1957e1ca537f2aefda79f1a1314eea470f66
Joseph Cheatham <jpchea03@gmail.com>
2024-11-20 00:48:04 -0500

    Added UI elements
    Added UI elements for score and pause menu, added start and game over screens.

--- Geo Runner/Assets/Scripts/PlayerScript.cs
+++ Geo Runner/Assets/Scripts/PlayerScript.cs
@@ -1,40 +1,75 @@
+// This script controls the player behavior
+
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float jumpForce;
+    public float jumpForce; // Force that the player jumps
+    public static int score; // Score achieved by player
 
     [SerializeField]
-    bool isGrounded = false;
+    bool isGrounded = false; // Determines if player is on ground for jumping
+    bool isAlive = true; // Determines if player is alive
 
-    Rigidbody2D rb;
+    Rigidbody2D rb; // Players rigidbody2D object
+    public TextMeshProUGUI scoreTxt; // Score UI element
 
+    // Initialize and track score, get rigid body component
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        score = 0;
+        StartCoroutine(IncrementScore());
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If space key is pressed...
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            // If player is on ground...
             if (isGrounded)
             {
-                rb.AddForce(Vector2.up * jumpForce);
-                isGrounded = false;
+                rb.AddForce(Vector2.up * jumpForce); // Jump!
+                isGrounded = false; // Player is no longer on ground
             }
         }
     }
 
+    // Method to increment score by 1 each second the player is alive
+    private System.Collections.IEnumerator IncrementScore()
+    {
+        // While the player is still alive
+        while (isAlive)
+        {
+            yield return new WaitForSeconds(1f); // Wait for 1 second
+            score += 1; // Increment score by 1
+            scoreTxt.text = "SCORE: " + score.ToString(); // Update the score text
+        }
+    }
+
+    // Controls player collisions
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("ground"))
+        // If player touches ground...
+        if (collision.gameObject.CompareTag("ground"))
         {
-            if(!isGrounded)
+            // If player is not already grounded...
+            if (!isGrounded)
             {
-                isGrounded=true;
+                isGrounded = true; // Ground player
             }
+        }
+
+        // If player touches spike...
+        if (collision.gameObject.CompareTag("spike"))
+        {
+            // End game
+            isAlive = false;
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
--- Geo Runner/Assets/Scripts/SpikeGenerator.cs
+++ Geo Runner/Assets/Scripts/SpikeGenerator.cs
@@ -1,29 +1,33 @@
-using System.CodeDom.Compiler;
+// This script controls the spawning of spikes and spike generator
+
 using UnityEngine;
 
 public class SpikeGenerator : MonoBehaviour
 {
 
-    public GameObject spike;
+    public GameObject spike; // Game object we are spawning
 
-    public float minSpeed;
-    public float maxSpeed;
-    public float curSpeed;
-    public float speedMultiplier;
+    public float minSpeed; // Minimum spike speed
+    public float maxSpeed; // Maximum spike speed
+    public float curSpeed; // Current speed of spikes
+    public float speedMultiplier; // Rate that speed increases
     
+    // Set current speed to min speed and spawn a spike
     void Awake()
     {
         curSpeed = minSpeed;
         GenerateSpike();
     }
 
+    // Method to generate spikes at random intervals
     public void GenerateSpikeRandom()
     {
-        float randomWait = Random.Range(0.1f, 0.2f);
+        float randomWait = Random.Range(0.1f, 1.0f);
         Invoke("GenerateSpike", randomWait);
 
     }
 
+    // Method to spawn spike
     void GenerateSpike()
     {
         GameObject SpikeIns = Instantiate(spike, transform.position, transform.rotation);
@@ -34,9 +38,10 @@ public class SpikeGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If current speed has not exceeded maximum...
         if (curSpeed < maxSpeed)
         {
-            curSpeed += speedMultiplier;
+            curSpeed += speedMultiplier; // Increase speed by speed multiplier
         }
     }
 }
